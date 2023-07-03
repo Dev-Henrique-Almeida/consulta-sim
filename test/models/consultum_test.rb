cdfc4990e5079c475da776fa57e8f0877baae3a1
Henrique <henrique.almeida.dev@gmail.com>
2023-07-03 16:05:52 -0300

    Corrigido os testes da consulta

--- test/models/consultum_test.rb
+++ test/models/consultum_test.rb
@@ -1,29 +1,23 @@
-require "test_helper"
+# test/models/consultum_test.rb
+require 'test_helper'
 
 class ConsultumTest < ActiveSupport::TestCase
   def setup
-    @medico = Medico.create(
-      nome: 'Dr. Fulano',
-      data_nascimento: Date.today,
-      cpf: '12345678910',
-      email: 'fulano@examplo.com',
-      especialidade: 'Cardiologia',
-      crm: '12345'
-    )
+    @medico = Medico.create(nome: 'Dr. João', data_nascimento: '1980-01-01', cpf: '12345678900',
+                            email: 'joao@example.com', especialidade: 'Cardiologia', crm: '12345')
+    @paciente = Paciente.create(nome: 'Maria', data_nascimento: '1990-01-01', cpf: '98765432100',
+                                email: 'maria@example.com', endereco: 'Rua A, 123')
+  end
 
-    @paciente = Paciente.create(
-      nome: 'Fulano de tal',
-      data_nascimento: Date.today,
-      cpf: '12345678910',
-      email: 'fulano01@gmail.com'
-    )
+  test "data should not be in the past" do
+    @consulta = Consultum.new(data: Date.yesterday, horario: '08:00', medico: @medico, paciente: @paciente)
+    assert_not @consulta.valid?
+    assert_includes @consulta.errors[:data], "Data não pode ser no passado!"
+  end
 
-    @consultum = Consultum.new(
-      data: Date.today,
-      horario: '08:00',
-      medico: @medico,
-      paciente: @paciente,
-    )
-
+  test "horario should be valid" do
+    @consulta = Consultum.new(data: Date.today, horario: '13:00', medico: @medico, paciente: @paciente)
+    assert_not @consulta.valid?
+    assert_includes @consulta.errors[:horario], "Horário inválido!"
   end
 end
