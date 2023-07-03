d0b78f386538d0c5e77ed07918e7e9038105738a
Henrique <henrique.almeida.dev@gmail.com>
2023-07-03 13:09:01 -0300

    Gerado os testes para consulta

--- test/models/consultum_test.rb
+++ test/models/consultum_test.rb
@@ -1,7 +1,29 @@
 require "test_helper"
 
 class ConsultumTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @medico = Medico.create(
+      nome: 'Dr. Fulano',
+      data_nascimento: Date.today,
+      cpf: '12345678910',
+      email: 'fulano@examplo.com',
+      especialidade: 'Cardiologia',
+      crm: '12345'
+    )
+
+    @paciente = Paciente.create(
+      nome: 'Fulano de tal',
+      data_nascimento: Date.today,
+      cpf: '12345678910',
+      email: 'fulano01@gmail.com'
+    )
+
+    @consultum = Consultum.new(
+      data: Date.today,
+      horario: '08:00',
+      medico: @medico,
+      paciente: @paciente,
+    )
+
+  end
 end
